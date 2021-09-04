e99de28afd04d904d15db5e63dc2abd720c29503
Bogdan Guna <bgguna@icloud.com>
2021-09-04 21:48:55 +0100

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Bogdan Guna on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
